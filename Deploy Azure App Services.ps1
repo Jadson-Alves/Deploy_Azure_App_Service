523c449847f0518fc46d768f331e1ad868c40e66
Jádson Alves <80630862+Jadson-Alves@users.noreply.github.com>
2021-09-05 23:00:10 +0000

    Add files via upload

---
+++ Deploy Azure App Services.ps1
@@ -0,0 +1,16 @@
+﻿# Variaveis
+$Location =”East US”
+$App1_Name = “AppService01”
+$RG_name = "RG-App_Services"
+$tier = "Standard"
+$Plan_name = "planAppService01"
+
+# Criar um grupo de recursos
+New-AzResourceGroup -Name $RGname -Location $Location1
+
+# Criar App Service Plan
+New-AzAppservicePlan -Name $Planname -ResourceGroupName $RGname -Location $Location -Tier $tier
+
+# Criar App Servie 
+New-AzWebApp -Name $App1Name -ResourceGroupName $RGname -Location $Location -AppServicePlan $Planname
+
